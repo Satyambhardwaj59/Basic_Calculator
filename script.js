3fee37ec94f81a04818aa8c16e45808301bca5ec
Satyambhardwaj59 <153222732+Satyambhardwaj59@users.noreply.github.com>
2024-11-20 13:38:07 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let string = "";
+
+let buttons = document.querySelectorAll('button');
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target);
+
+        string += e.target.innerHTML;
+        document.querySelector('input').value = string
+        
+    })
+})
+
+
+
+
+
