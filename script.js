f66769f09d83347ea9f67e705782797e8f462862
Satyambhardwaj59 <153222732+Satyambhardwaj59@users.noreply.github.com>
2024-11-21 19:40:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-
 let string = "";
-
-let buttons = document.querySelectorAll('button');
-
-Array.from(buttons).forEach((button) => {
+const display = document.querySelector('#input-display');
+           
+document.querySelectorAll('button').forEach((button) => {
     button.addEventListener('click', (e) => {
-        console.log(e.target);
-
-        string += e.target.innerHTML;
-        document.querySelector('input').value = string
+    const value = e.target.innerHTML;
         
-    })
-})
-
-
-
-
-
+        if (value === "AC") {
+                    // Clear the display
+                    string = "";
+                    display.value = string;
+         } else if (value === "DE") {
+        
+            // Delete the last character
+            string = string.slice(0, -1);
+            display.value = string;
+        } else if (value === "=") {
+            try {
+ 
+           // Evaluate the expression
+            string = eval(string).toString();
+            display.value = string;
+         } catch {
+            display.value = "Error";
+            string = "";
+        }
+    } else {
+        // Append the button value to the string
+        string += value;
+        display.value = string;
+    }
+});
+});
